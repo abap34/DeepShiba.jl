986b7f09eb36c1d25ea5198b6befc507d702b700
abap34 <uchi_ymgc@her0.jp>
2020-05-19 18:58:11 +0900

    :up: Update runtest.jl; add simple DiffTest

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
 using Test
+using DeepShiba
+#include("../src/DeepShiba.jl")
 
-@testset "TravisTest" begin
-    1 + 1 == 2
+const e = 10e-5
+@testset "DiffTest" begin
+    x = Variable(0.5, nothing, nothing)
+    f = Func(
+        x -> x ^ 2,
+        x -> 2x,
+        nothing,
+        nothing
+    )
+    y = f(x)
+    y.grad = 1
+    backward!(y)
+    @test 1.0 - e <= x.grad <= 1.0 + e
 end
