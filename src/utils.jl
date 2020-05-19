d7c33b0458c57cb954303979e5803d0a4d7c7e22
abap34 <uchi_ymgc@her0.jp>
2020-05-19 19:01:11 +0900

    :hammer: Create utils.jl; Add function to output Variable and Function as text.

---
+++ src/utils.jl
@@ -0,0 +1,21 @@
+include("core_simple.jl")
+function Base.print(x::Variable,debug=false)
+    print("Variable(")
+    print("data:",x.data)
+    if debug
+        println()
+        (x.grad !== nothing) && println("grad:",x.grad)
+        (x.creator !== nothing) && println("creator:",x.creator.name)
+    end
+    println(")")
+end
+
+function Base.print(x::Func)
+    println("Func[")
+    print("input: ")
+    println(x.input)
+    print("output: ")
+    println(x.output)
+    println("]")
+end
+
