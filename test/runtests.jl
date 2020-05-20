93fb084272e84d50dff3444fb192e983f9106140
abap34 <uchi_ymgc@her0.jp>
2020-05-20 16:31:48 +0900

    :up: Update runtests.jl; Add tests for numerical differentiation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,22 @@
 using Test
 using DeepShiba
-#include("../src/DeepShiba.jl")
+
 
 const e = 10e-5
+
+@testset "NumericalDiffTest" begin
+    f(x) = 2x^3 + x^2 + 3x + 2
+    f′(x) = 6x^2 + 2x + 3
+    for i in 0:0.1:1
+        @test (f′(i) - e) <= (numerical_diff(f, i)) <= (f′(i) + e) 
+    end
+end
+
 @testset "DiffTest" begin
     x = Variable(0.5, nothing, nothing)
     f = Func(
-        x -> x ^ 2,
-        x -> 2x,
+        x->x^2,
+        x->2x,
         nothing,
         nothing
     )
