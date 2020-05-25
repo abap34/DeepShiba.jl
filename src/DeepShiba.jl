5778abbb0df37a85c96775810011c747c02f8992
abap34 <uchi_ymgc@her0.jp>
2020-05-25 18:36:31 +0900

    :up: Update core_simpe.jl, DeepShiba.jl; Fix a bug in the order in which the functions were retrieved.

--- src/DeepShiba.jl
+++ src/DeepShiba.jl
@@ -1,5 +1,5 @@
 module DeepShiba
     include("core_simple.jl")
     include("utils.jl")
-    export  Variable, Func, variable, func, backward!, numerical_diff, cleargrad
+    export  Variable, Func, variable, func, backward!, numerical_diff, cleargrad, print
 end 
