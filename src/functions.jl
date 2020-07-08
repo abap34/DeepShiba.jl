9b1d52e8a314183ccaaa5d1d688ad6d69c8fef33
abap34 <uchi_ymgc@her0.jp>
2020-07-08 22:40:54 +0900

    Update functions.jl; Log, Tan, Tanh

--- src/functions.jl
+++ src/functions.jl
@@ -3,6 +3,13 @@ import Base
 
 # ----MathFunc----
 
+
+mutable struct Log <: Func
+    inputs
+    outputs
+    generation
+end
+
 mutable struct Sin <: Func
     inputs
     outputs
@@ -15,26 +22,64 @@ mutable struct Cos <: Func
     generation
 end
 
+mutable struct Tan <: Func
+    inputs
+    outputs
+    generation
+end
+
+mutable struct Tanh <: Func
+    inputs
+    outputs
+    generation
+end
+
+
+_Log(x::Variable) = Log(nothing, nothing, nothing)(x)
 _Sin(x) = Sin(nothing, nothing, nothing)(x)
 _Cos(x) = Cos(nothing, nothing, nothing)(x)
+_Tan(x) = Tan(nothing, nothing, nothing)(x)
+_Tanh(x) = Tanh(nothing, nothing, nothing)(x)
 
+forward(f::Log, x) = log(x)
 forward(f::Sin, x) = sin(x)
 forward(f::Cos, x) = cos(x)
+forward(f::Tan, x) = tan(x)
+forward(f::Tanh, x) = tanh(x)
 
 
-function backward(f::Cos)
+function backward(f::Log) :: AbstractArray{Variable} 
     gys = get_gys(f.outputs)
-    x, = f.inputs
-    return (-sin(x),) .* gys
-end
-
-function backward(f::Sin)
-    gys = get_gys(f.outputs)
-    x, = f.inputs
-    return (cos(x),) .* gys
+    x = f.inputs[1]
+    return [1 / x,] .* gys
 end
 
 
+function backward(f::Sin) :: AbstractArray{Variable} 
+    gys = get_gys(f.outputs)
+    x, = f.inputs
+    return [cos(x),] .* gys
+end
+
+function backward(f::Cos) :: AbstractArray{Variable} 
+    gys = get_gys(f.outputs)
+    x, = f.inputs
+    return [-sin(x),] .* gys
+end
+
+
+function backward(f::Tan) :: AbstractArray{Variable} 
+    gys = get_gys(f.outputs)
+    x, = f.inputs
+    return [1 / cos(x)^2,] .* gys
+end
+
+
+function backward(f::Tanh) :: AbstractArray{Variable} 
+    gys = get_gys(f.outputs)
+    y = f.outputs[1]
+    return [1 - y ^ 2,] .* gys
+end
 
 
 
@@ -63,3 +108,5 @@ Base.reshape(x, shape::Variable) = _Reshape(variable(x), shape)
 
 Base.sin(x::Variable) = _Sin(x)
 Base.cos(x::Variable) = _Cos(x)
+Base.tan(x::Variable) = _Tan(x)
+Base.tanh(x::Variable) = _Tanh(x)
