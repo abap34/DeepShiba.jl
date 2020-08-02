1451902ff8dba5f09f26ab4a24f54814cc3b62b3
abap34 <uchi_ymgc@her0.jp>
2020-08-02 23:55:46 +0900

    Update functions.jl; core_structures.jl; change the type of structure faster

--- src/core_structures.jl
+++ src/core_structures.jl
@@ -6,115 +6,112 @@ abstract type ShibaObject end
 abstract type Func <: ShibaObject end 
 
 
-NullableRealValueType = Union{Real,AbstractArray{<:Real}, Nothing}
-NullableFuncType = Union{Func,Nothing}
-NullableVariableValueType = Any
-
+const NullableRealValueType = Union{Real,AbstractArray{<:Real},Nothing}
+const NullableFuncType = Union{Func,Nothing}
 
 mutable struct Variable <: ShibaObject 
-    data :: NullableRealValueType
-    creator :: NullableFuncType
-    grad :: NullableVariableValueType
-    generation :: Int
-    name :: String
+    data::NullableRealValueType
+    creator::NullableFuncType
+    grad::Union{Variable,AbstractArray{Variable},Nothing}
+    generation::Int
+    name::String
 end
 
-NullableVariableValueType = Union{Variable,AbstractArray{Variable}, Nothing}
+const NullableVariableArray = Union{AbstractArray{Variable},Nothing}
 
 mutable struct Add <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 
 mutable struct Sub <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 
 mutable struct Neg <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 
 mutable struct Mul <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 
 mutable struct Div <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 
 mutable struct Pow <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
-    c :: NullableRealValueType
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
+    c::NullableRealValueType
 end
 
 
 
 
-_Add(x1, x2) =  Add(nothing, nothing, 0)(x1, x2)
+_Add(x1, x2) =  Add([x1, x2], nothing, 0)(x1, x2)
 
-_Sub(x1, x2) = Sub(nothing, nothing, 0)(x1, x2)
+_Sub(x1, x2) = Sub([x1, x2], nothing, 0)(x1, x2)
 
-_Neg(x) = Neg(nothing, nothing, 0)(x)
+_Neg(x) = Neg([x, ], nothing, 0)(x)
 
-_Mul(x1, x2) = Mul(nothing, nothing, 0)(x1, x2)
+_Mul(x1, x2) = Mul([x1, x2], nothing, 0)(x1, x2)
 
-_Div(x1, x2) = Div(nothing, nothing, 0)(x1, x2)
+_Div(x1, x2) = Div([x1, x2], nothing, 0)(x1, x2)
 
-_Pow(x, c) = Pow(nothing, nothing, 0, c)(x)
+_Pow(x, c) = Pow([x, ], nothing, 0, c)(x)
 
 
+@inline forward(f::Add, x1, x2)  = x1 + x2
+@inline forward(f::Sub, x1, x2) = x1 - x2
+@inline forward(f::Neg, x) = -x
+@inline forward(f::Mul, x1, x2) = x1 * x2
+@inline forward(f::Div, x1, x2) = x1 / x2
+@inline forward(f::Pow, x) = x^(f.c) 
 
-forward(f::Add, x1, x2)  = x1 + x2
-forward(f::Sub, x1, x2) = x1 - x2
-forward(f::Neg, x) = -x
-forward(f::Mul, x1, x2) = x1 * x2
-forward(f::Div, x1, x2) = x1 / x2
-forward(f::Pow, x) = x^(f.c)
-
-function backward(f::Add) :: AbstractArray{Variable} 
+function backward(f::Add)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
-    return gys .* [1, 1]
+    return  [1, 1] .* gys
 end
 
-function backward(f::Sub) :: AbstractArray{Variable} 
+function backward(f::Sub)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
-    return gys .* [1, -1]
+    return  [1, -1] .* gys
 end
 
-function backward(f::Neg) :: AbstractArray{Variable} 
+function backward(f::Neg)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
     return -gys
 end
 
-function backward(f::Mul) :: AbstractArray{Variable} 
+function backward(f::Mul)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
     x1, x2 = f.inputs
     return [x2, x1] .* gys
 end
 
-function backward(f::Div) :: AbstractArray{Variable} 
+function backward(f::Div)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
     x1, x2 = f.inputs
     return [1 / x2, -x1 / (x2^2)] .* gys
 end
 
-function backward(f::Pow) :: AbstractArray{Variable} 
+function backward(f::Pow)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
     x, c = f.inputs[1], f.c    
     return [c * (x^(c - 1)),] .* gys
@@ -147,6 +144,5 @@ Base.:^(x1, x2::Variable) = _Pow(variable(x1), x2.data)
 
 Base.log(x::Variable) = _Log(x)
 Base.size(x::Variable) = size(x.data)
-
-
+Base.iterate(x::Variable) = iterate(x.data)
 
--- src/functions.jl
+++ src/functions.jl
@@ -5,33 +5,33 @@ import Base
 
 
 mutable struct Log <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 mutable struct Sin <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 mutable struct Cos <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 mutable struct Tan <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 mutable struct Tanh <: Func
-    inputs :: NullableVariableValueType
-    outputs :: NullableVariableValueType
-    generation :: Int
+    inputs::NullableVariableArray
+    outputs::NullableVariableArray
+    generation::Int
 end
 
 
@@ -48,37 +48,37 @@ forward(f::Tan, x) = tan(x)
 forward(f::Tanh, x) = tanh(x)
 
 
-function backward(f::Log) :: AbstractArray{Variable} 
+function backward(f::Log)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
     x = f.inputs[1]
     return [1 / x,] .* gys
 end
 
 
-function backward(f::Sin) :: AbstractArray{Variable} 
+function backward(f::Sin)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
     x, = f.inputs
     return [cos(x),] .* gys
 end
 
-function backward(f::Cos) :: AbstractArray{Variable} 
+function backward(f::Cos)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
     x, = f.inputs
     return [-sin(x),] .* gys
 end
 
 
-function backward(f::Tan) :: AbstractArray{Variable} 
+function backward(f::Tan)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
     x, = f.inputs
     return [1 / cos(x)^2,] .* gys
 end
 
 
-function backward(f::Tanh) :: AbstractArray{Variable} 
+function backward(f::Tanh)::AbstractArray{Variable} 
     gys = get_gys(f.outputs)
     y = f.outputs[1]
-    return [1 - y ^ 2,] .* gys
+    return [1 - y^2,] .* gys
 end
 
 
@@ -89,24 +89,44 @@ mutable struct Reshape <: Func
     inputs
     outputs
     generation
+    shape
 end
 
-_Reshape(x, shape) = Reshape(nothing, nothing, nothing)(x, shape)
 
-forward(f::Reshape, x, shape)  = reshape(x, shape)
+
+mutable struct Transpose <: Func
+    inputs
+    outputs
+    generation
+end
+
+_Reshape(x, shape) = Reshape(nothing, nothing, nothing, shape)(x)
+_Transpose(x) = Transpose(nothing, nothing, nothing)(x)
+
+
+forward(f::Reshape, x)  = reshape(x, f.shape)
+forward(f::Transpose, x) = transpose(x)
 
 function backward(f::Reshape)
-    gys = f.outputs[1].grad
-    return (reshape(gys, size(f.inputs[1])),)
+    gys = get_gys(f.outputs)
+    x_shape = size(f.inputs[1])  
+    return reshape.(gys, Ref(x_shape))
+end
+
+function backward(f::Transpose)
+    gys = get_gys(f.outputs)
+    return transpose.(gys)
 end
 
 
 
-Base.reshape(x::Variable, shape::Variable) = _Reshape(x, shape)
-Base.reshape(x::Variable, shape) = _Reshape(x, variable(shape))
-Base.reshape(x, shape::Variable) = _Reshape(variable(x), shape)
 
 Base.sin(x::Variable) = _Sin(x)
 Base.cos(x::Variable) = _Cos(x)
 Base.tan(x::Variable) = _Tan(x)
 Base.tanh(x::Variable) = _Tanh(x)
+
+Base.reshape(x::Variable, shape::Variable) = _Reshape(x, shape.data)
+Base.reshape(x::Variable, shape) = _Reshape(x, shape)
+Base.reshape(x, shape::Variable) = _Reshape(variable(x), shape.data)
+Base.transpose(x::Variable) = _Transpose(x)
