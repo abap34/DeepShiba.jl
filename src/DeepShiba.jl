53973d251590ca38d53dcd21c112f7f35649802b
abap34 <uchi_ymgc@her0.jp>
2020-08-27 22:43:09 +0900

    Update DeepShiba.jl; add logo

--- src/DeepShiba.jl
+++ src/DeepShiba.jl
@@ -1,4 +1,13 @@
 module DeepShiba
+    logo = raw"
+ ____  _____ _____ ____  ____  _   _ ___ ____    _    
+|  _ \| ____| ____|  _ \/ ___|| | | |_ _| __ )  / \    |  Documentation: https://xxxxxxxxxx
+| | | |  _| |  _| | |_) \___ \| |_| || ||  _ \ / _ \   |  
+| |_| | |___| |___|  __/ ___) |  _  || || |_) / ___ \  |  Version: v0.1 (yyyy-mm-dd)
+|____/|_____|_____|_|   |____/|_| |_|___|____/_/   \_\ |
+"
+    
+    (isinteractive()) && (println(logo))                                                  
     include("core/core_structures.jl")
     include("core/core_functions.jl")
     include("utils/output_txt.jl")
